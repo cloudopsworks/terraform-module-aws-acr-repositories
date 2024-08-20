b6d15d8b6c251ea27aa6a08a8b1ecf09224fac9f
Cristian Beraha <cristian@cloudops.works>
2024-08-20 18:50:49 -0300

    chore: updated entries

--- main.tf
+++ main.tf
@@ -12,10 +12,15 @@ locals {
 resource "aws_ecr_repository" "this" {
   for_each             = local.repos
   name                 = each.value.name
+  force_delete         = try(each.value.force_delete, false)
   image_tag_mutability = try(each.value.image_tag_mutability, "IMMUTABLE")
   encryption_configuration {
     encryption_type = var.default_kms_key != "" ? "KMS" : try(each.value.encryption_type, "AES256")
     kms_key         = try(each.value.kms_key, var.default_kms_key)
   }
+  image_scanning_configuration {
+    scan_on_push = try(each.value.scan_on_push, false)
+  }
+
   tags = local.all_tags
 }
